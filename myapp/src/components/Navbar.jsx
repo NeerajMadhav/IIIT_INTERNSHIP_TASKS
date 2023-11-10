59a20433d16fe6784c40e9eb89f161ffc5a364da
NeerajMadhav <neerajmadhav777@gmail.com>
2023-11-10 10:48:07 +0530

    modified:   myapp/src/App.js 	modified:   myapp/src/components/Navbar.jsx 	modified:   myapp/src/styles/Navbar.css

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -1,1 +1,16 @@
-//sfjkdsfh
+import React from 'react';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">StartUp</div>
+      <div className="navbar__spacer"></div>
+      <div className="navbar__search">
+        <input type="text" placeholder="Search..." />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
